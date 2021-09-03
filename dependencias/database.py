1e286a3c5792f87e9d70ee924f33a0ddb4ba44eb
grigio888 <57846373+grigio888@users.noreply.github.com>
2021-09-03 20:36:12 -0300

    v0.0.1
    Ajustando a estrutura do Banco.

--- dependencias/database.py
+++ dependencias/database.py
@@ -19,14 +19,16 @@ class FerramentaDatabase():
     def auto_iniciar(self):
         relacao = {  
             'login'     : 'login text primary key, senha text, ultimo_login integer, criado_em integer, nome_utilizador text, endereco_utilizador text',
-            'cliente'   : 'nome text primary key, endereco text, tel_1 integer, tel_2 integer',
-            'servico'   : 'cliente text, n_pedido integer primary key, modo integer, id_orcamento integer, foreign key (cliente) references cliente (nome)',
-            'itens'     : 'item_id integer primary key autoincrement, nome text, preco integer',
-            'orcamento' : 'id_orcamento integer, item integer, data integer, foreign key (id_sacola) references pedido (id_sacola), foreign key (item) references itens (item_id)',
+            'cliente'   : 'id_cliente int primary key, nome text, identidade int primary key, endereco text, tel_1 integer, tel_2 integer',
+            'servico'   : 'id_cliente int, id_pedido integer primary key, modo integer, id_orcamento integer primary key, foreign key (id_cliente) references cliente (id_cliente)',
+            'itens'     : 'id_item integer primary key autoincrement, nome text, preco integer',
+            'orcamento' : 'id_orcamento integer, item integer, data integer, foreign key (id_orcamento) references pedido (id_orcamento), foreign key (item) references itens (id_item)',
         }
 
         for item in relacao.keys():
             self.executando(f'create table if not exists {item} ({relacao[item]})')
+
+#  TODO: 
 
 if __name__ == "__main__":
 
@@ -39,7 +41,7 @@ if __name__ == "__main__":
     #db.executando('insert into cliente values ("Angolano", "Angola", "5527998881234", "0")')
     #db.executando('insert into itens values ("1000", "Troca de Algo", 15000)')
     
-    db
+    #db
 
     #print(db.executando('select * from login', True, True))
     #db.executando('delete from login')
--- dependencias/engine.py
+++ dependencias/engine.py
@@ -2,21 +2,29 @@ from database import FerramentaDatabase
 
 class Esqueleto():
     def __init__(self):
-        self.database = FerramentaDatabase()
+        self.auto_iniciar()
         self.tabela = 'teste'
 
-    def cadastrar(self, tabela, lista_valores):
-        comando = f'insert into {self.tabela} values ({lista_valores})'
+    def auto_iniciar(self):
+        self.database = FerramentaDatabase()
+
+    def cadastrar(self, lista_valores):
+        tratado = ''
+
+        for item in lista_valores:
+            if item == lista_valores[len(lista_valores) - 1]: tratado = tratado + f'"{item}"'
+            else: tratado = tratado + f'"{item}", '
+        
+        comando = f'insert into {self.tabela} values ({tratado})'
         self.database.executando(comando)
 
-    #def exibir(self, itens, item_especifico = False, lista_local):
-    #    comando = f'select {itens} from {self.tabela}'
-    #    if item_especifico: comando = f'{comando} where {lista_local[0]} = {lista_local[1]}'
-    #    self.database.executando(comando, True)
-    #    return self.database.executando(comando, True, True)
+    def exibir(self, itens, lista_local, item_especifico = False):
+        comando = f'select {itens} from {self.tabela}'
 
-    def exibir(self):
-        pass
+        if item_especifico: comando = f'{comando} where {lista_local[0]} = {lista_local[1]}'
+
+        self.database.executando(comando, True)
+        return self.database.executando(comando, True, True)
 
     def atualizar(self, dict_valores, lista_local):
         valores = [f'{item} = {dict_valores[item]}' for item in dict_valores.keys()]
@@ -28,35 +36,33 @@ class Esqueleto():
         comando = f'delete from {self.tabela} where {lista_local[0]} = {lista_local[1]})'
         self.database.executando(comando)
 
-
-class CuidadosCliente(Esqueleto):
+class TratandoLogin(Esqueleto):
     def __init__(self):
-        super().__init__()
+        self.auto_iniciar()
+        self.tabela = 'login'
 
-#class CRUD_Organizador():
-#
-#    #clientes
-#
-#    def cadastrar_cliente(self):
-#
-#    def listar_clientes(self, id):
-#        #se o id for informado, buscar especifico
-#
-#    def atualizar_cliente(self):
-#
-#    def remover_cliente(self):
-#
-#    #conta_login
-#
-#    def cadastrar_conta(self):
-#    def listar_contas(self, id):
-#        #se o id for informado, buscar especifico
-#    def atualizar_conta(self):
-#    def remover_conta(self):
-#
-#    #servicos
-#
-#    def cadastrar_servico(self):
-#    def listar_servicos(self, id):
-#    def atualizar_servicos
-#    
+class TratandoCliente(Esqueleto):
+    def __init__(self):
+        self.auto_iniciar()
+        self.tabela = 'cliente'
+
+class TratandoServicos(Esqueleto):
+    def __init__(self):
+        self.auto_iniciar()
+        self.tabela = 'servico'
+
+class TratandoItens(Esqueleto):
+    def __init__(self):
+        self.auto_iniciar()
+        self.tabela = 'itens'
+
+class TratandoOrcamento(Esqueleto):
+    def __init__(self):
+        self.auto_iniciar()
+        self.tabela = 'itens'
+
+if __name__ == '__main__':
+
+    teste = TratandoCliente()
+    #teste.atualizar(['Macaco', 'Floresta', '552233226655', '552233665511'])
+    print(teste.exibir('rowid, *', ''))
